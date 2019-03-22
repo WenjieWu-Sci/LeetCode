d16f14090d6588fea4b8e4670cc0e0df38d2846d
WenjieWu <whuwenjie@gmail.com>
2019-03-22 23:21:40 +0800

    7 Reverse Integer

---
+++ 7_ReverseInteger/solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+class Solution {
+public:
+    int reverse(int x) {
+        int rev= 0;
+        while (x!=0) {
+            int pop= x%10;
+            x/= 10;
+            if (rev>INT_MAX/10 || (rev==INT_MAX/10 && pop>7)) return 0;
+            if (rev<INT_MIN/10 || (rev==INT_MIN/10 && pop<-8)) return 0;
+            rev= rev*10+pop;
+        }
+        return rev;
+    }
+};
+
+int main(int argc, char** argv) {
+    int org_num= 1234;
+    if (argc>1) org_num= atoi(argv[1]);
+    Solution rev;
+    int rev_num= rev.reverse(org_num);
+    std::cout<<org_num<<"<--->"<<rev_num<<std::endl;
+    return 0;
+}
